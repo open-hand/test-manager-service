2b26c77fa6046d83b0ce7f795a3c3e047bd8e39d
root <root@devops-service-79fccb8785-vsrd9>
2018-06-08 09:31:18 +0800

    Render Variables[skip ci]

--- src/main/java/com/test/devops/service/DemoService.java
+++ src/main/java/com/test/devops/service/DemoService.java
@@ -3,3 +3,4 @@ package com.test.devops.service;
 public interface DemoService {
     String hello();
 }
+
--- src/main/java/com/test/devops/service/impl/DemoServiceImpl.java
+++ src/main/java/com/test/devops/service/impl/DemoServiceImpl.java
@@ -11,3 +11,4 @@ public class DemoServiceImpl implements DemoService {
         return "Hello World";
     }
 }
+
