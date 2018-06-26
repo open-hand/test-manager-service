3ecf941c5eea736291ce55b8c8b1621d50db4042
Jerome_Zuo <842767365@qq.com>
2018-06-26 13:51:48 +0800

    测试模块

--- src/main/java/io/choerodon/test/manager/domain/service/impl/ITestCycleCaseDefectRelServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/domain/service/impl/ITestCycleCaseDefectRelServiceImpl.java
@@ -2,34 +2,37 @@ package io.choerodon.test.manager.domain.service.impl;
 
 import io.choerodon.test.manager.domain.test.manager.entity.TestCycleCaseDefectRelE;
 import io.choerodon.test.manager.domain.service.ITestCycleCaseDefectRelService;
-import io.choerodon.core.domain.Page;
-import io.choerodon.mybatis.pagehelper.domain.PageRequest;
+import io.choerodon.test.manager.domain.test.manager.factory.TestCycleCaseDefectRelEFactory;
+import io.choerodon.test.manager.infra.feign.TestCaseFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * Created by jialongZuo@hand-china.com on 6/11/18.
+ * Created by 842767365@qq.com on 6/11/18.
  */
 @Component
 public class ITestCycleCaseDefectRelServiceImpl implements ITestCycleCaseDefectRelService {
+
+	@Autowired
+	TestCaseFeignClient testCaseFeignClient;
+
+	@Transactional(rollbackFor = Exception.class)
 	@Override
 	public TestCycleCaseDefectRelE insert(TestCycleCaseDefectRelE testCycleCaseDefectRelE) {
 		return testCycleCaseDefectRelE.addSelf();
 	}
 
+	@Transactional(rollbackFor = Exception.class)
 	@Override
 	public void delete(TestCycleCaseDefectRelE testCycleCaseDefectRelE) {
 		testCycleCaseDefectRelE.deleteSelf();
 	}
 
-//	@Override
-//	public List<TestCycleCaseDefectRelE> update(List<TestCycleCaseDefectRelE> testCycleCaseDefectRelE) {
-//		List<TestCycleCaseDefectRelE> testCaseES = new ArrayList<>();
-//		testCycleCaseDefectRelE.forEach(v -> testCaseES.add(v.updateSelf()));
-//		return testCaseES;
-//	}
+
 
 	@Override
 	public List<TestCycleCaseDefectRelE> query(TestCycleCaseDefectRelE testCycleCaseDefectRelE) {
@@ -38,10 +41,14 @@ public class ITestCycleCaseDefectRelServiceImpl implements ITestCycleCaseDefectR
 
 	@Override
 	public List<TestCycleCaseDefectRelE> query(Long linkId, String defectType) {
-//		TestCycleCaseDefectRelE testCycleCaseDefectRelE=TestCycleCaseDefectRelEFactory.create();
-//		testCycleCaseDefectRelE.setDefectLinkId(linkId);
-//		testCycleCaseDefectRelE.setDefectType(defectType);
-//		testCycleCaseDefectRelE.querySelf()
-		return null;
+		TestCycleCaseDefectRelE testCycleCaseDefectRelE = TestCycleCaseDefectRelEFactory.create();
+		testCycleCaseDefectRelE.setDefectLinkId(linkId);
+		testCycleCaseDefectRelE.setDefectType(defectType);
+		List<TestCycleCaseDefectRelE> lists = testCycleCaseDefectRelE.querySelf();
+		List<Long> issueLists = lists.stream().map(v -> v.getIssueId()).collect(Collectors.toList());
+		//testCaseFeignClient
+		//获取issueName
+		//插入返回值
+		return lists;
 	}
 }
