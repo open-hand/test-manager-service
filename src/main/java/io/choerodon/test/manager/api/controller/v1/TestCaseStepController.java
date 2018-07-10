74fb4bda70b4554656700331475dc96b8375a512
Jerome_Zuo <842767365@qq.com>
2018-07-10 19:21:38 +0800

    testcycle 查询日期

--- src/main/java/io/choerodon/test/manager/api/controller/v1/TestCaseStepController.java
+++ src/main/java/io/choerodon/test/manager/api/controller/v1/TestCaseStepController.java
@@ -1,5 +1,6 @@
 package io.choerodon.test.manager.api.controller.v1;
 
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.test.manager.api.dto.TestCaseStepDTO;
 import io.choerodon.test.manager.app.service.TestCaseStepService;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by 842767365@qq.com on 6/11/18.
@@ -21,12 +25,15 @@ public class TestCaseStepController {
     TestCaseStepService iTestCaseStepService;
 
 
-//    @Permission(level = ResourceLevel.PROJECT)
-//    @ApiOperation("批量变动测试步骤(添加|修改)")
-//    @PutMapping("/changes")
-//    public boolean changeStep(@RequestBody List<TestCaseStepDTO> testCaseStepDTO) {
-//        return iTestCaseStepService.changeStep(testCaseStepDTO);
-//    }
+	@Permission(level = ResourceLevel.PROJECT)
+	@ApiOperation("批量变动测试步骤(添加|修改)")
+	@PutMapping("/batchInsert")
+	public ResponseEntity<List<TestCaseStepDTO>> changeStep(@RequestBody List<TestCaseStepDTO> testCaseStepDTO) {
+
+		return Optional.ofNullable(iTestCaseStepService.batchInsertStep(testCaseStepDTO))
+				.map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
+				.orElseThrow(() -> new CommonException("error.testCycleCase.query.cycleId"));
+	}
 
     @Permission(level = ResourceLevel.PROJECT)
     @ApiOperation("变动一个测试步骤(添加|修改)")
