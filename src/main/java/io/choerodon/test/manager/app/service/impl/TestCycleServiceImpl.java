01fef362db8a2630102fbdf35f5e09e27ceffef2
Jerome_Zuo <842767365@qq.com>
2018-07-13 13:35:48 +0800

    ``

--- src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleServiceImpl.java
@@ -85,14 +85,8 @@ public class TestCycleServiceImpl implements TestCycleService {
 
 	@Override
 	public JSONObject getTestCycle(Long projectId) {
-		//ResponseEntity<List<ProductVersionDTO>> dto = productionVersionClient.listByProjectId(projectId);
-		//List<ProductVersionDTO> versions = dto.getBody();
-		List<ProductVersionDTO> versions = new ArrayList<>();
-		ProductVersionDTO v1 = new ProductVersionDTO();
-		v1.setStatusName("111");
-		v1.setName("1111");
-		v1.setVersionId(new Long(167));
-		versions.add(v1);
+		ResponseEntity<List<ProductVersionDTO>> dto = productionVersionClient.listByProjectId(projectId);
+		List<ProductVersionDTO> versions = dto.getBody();
 		if (versions.size() == 0) {
 			return new JSONObject();
 		}
