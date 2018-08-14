0caa372d9c3abff71caa8fd245f062d692f81e04
Jerome_Zuo <842767365@qq.com>
2018-08-14 15:12:27 +0800

    saga消息消费修复

--- src/main/java/io/choerodon/test/manager/api/eventhandler/TestManagerEventHandler.java
+++ src/main/java/io/choerodon/test/manager/api/eventhandler/TestManagerEventHandler.java
@@ -29,89 +29,87 @@ import java.io.IOException;
 @Component
 public class TestManagerEventHandler {
 
-    @Autowired
-    private TestCycleService testCycleService;
+	@Autowired
+	private TestCycleService testCycleService;
 
-    @Autowired
-    private TestCycleCaseService testCycleCaseService;
+	@Autowired
+	private TestCycleCaseService testCycleCaseService;
 
-    @Autowired
-    private TestCaseStepService testCaseStepService;
+	@Autowired
+	private TestCaseStepService testCaseStepService;
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestManagerEventHandler.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestManagerEventHandler.class);
 
-    private void loggerInfo(Object o) {
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.info("data: {}", o);
-        }
-    }
+	private void loggerInfo(Object o) {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.info("data: {}", o);
+		}
+	}
 
-    /**
-     * 创建临时循环事件
-     *
-     * @param message
-     */
+	/**
+	 * 创建临时循环事件
+	 *
+	 * @param message
+	 */
 	@SagaTask(code = "test-create-version",
 			description = "创建临时循环事件",
 			sagaCode = "agile-create-version",
 			//enabledDbRecord = true,
 			seq = 1)
-	public void handleProjectVersionCreateEvent(String message) throws IOException {
-		EventPayload<VersionEvent> payload = objectMapper.readValue(message, EventPayload.class);
-		VersionEvent versionEvent = payload.getData();
-        loggerInfo(versionEvent);
-        TestCycleDTO testCycleDTO = new TestCycleDTO();
-        testCycleDTO.setVersionId(versionEvent.getVersionId());
-        testCycleDTO.setType(TestCycleE.TEMP);
-        testCycleDTO.setCycleName("临时");
-        testCycleService.insert(testCycleDTO);
-    }
+	public VersionEvent handleProjectVersionCreateEvent(String message) throws IOException {
+		VersionEvent versionEvent = objectMapper.readValue(message, VersionEvent.class);
+		loggerInfo(versionEvent);
+		TestCycleDTO testCycleDTO = new TestCycleDTO();
+		testCycleDTO.setVersionId(versionEvent.getVersionId());
+		testCycleDTO.setType(TestCycleE.TEMP);
+		testCycleDTO.setCycleName("临时");
+		testCycleService.insert(testCycleDTO);
+		return versionEvent;
+	}
 
-    /**
-     * 版本删除事件
-     *
-     * @param message
-     */
+	/**
+	 * 版本删除事件
+	 *
+	 * @param message
+	 */
 	@SagaTask(code = "test-delete-version",
 			description = "删除version事件，删除相关测试数据",
 			sagaCode = "agile-delete-version",
 			//enabledDbRecord = true,
 			seq = 1)
-    public void handleProjectVersionDeleteEvent(String message) throws IOException {
-		EventPayload<VersionEvent> payload = objectMapper.readValue(message, EventPayload.class);
+	public VersionEvent handleProjectVersionDeleteEvent(String message) throws IOException {
+		VersionEvent versionEvent = objectMapper.readValue(message, VersionEvent.class);
+		loggerInfo(versionEvent);
+		TestCycleDTO testCycleDTO = new TestCycleDTO();
+		testCycleDTO.setVersionId(versionEvent.getVersionId());
+		testCycleService.delete(testCycleDTO, versionEvent.getProjectId());
+		return versionEvent;
+	}
 
-		VersionEvent versionEvent = payload.getData();
-        loggerInfo(versionEvent);
-        TestCycleDTO testCycleDTO =	new TestCycleDTO();
-        testCycleDTO.setVersionId(versionEvent.getVersionId());
-          testCycleService.delete(testCycleDTO,versionEvent.getProjectId());
-    }
-
-    /**
-     * 问题删除事件
-     *
-     * @param message
-     */
-    @SagaTask(code = "test-delete-issue",
-            description = "删除issue事件，删除相关测试数据",
-            sagaCode = "agile-delete-issue",
-           // enabledDbRecord = true,
-            seq = 1)
-    public void handleProjectIssueDeleteEvent(String message) throws IOException {
-		EventPayload<IssuePayload> payload = objectMapper.readValue(message, EventPayload.class);
-
-		IssuePayload issuePayload = payload.getData();
-        TestCycleCaseDefectRelE defectRelE=TestCycleCaseDefectRelEFactory.create();
-        defectRelE.setIssueId(issuePayload.getIssueId());
-        defectRelE.deleteSelf();
+	/**
+	 * 问题删除事件
+	 *
+	 * @param message
+	 */
+	@SagaTask(code = "test-delete-issue",
+			description = "删除issue事件，删除相关测试数据",
+			sagaCode = "agile-delete-issue",
+			// enabledDbRecord = true,
+			seq = 1)
+	public IssuePayload handleProjectIssueDeleteEvent(String message) throws IOException {
+		IssuePayload issuePayload = objectMapper.readValue(message, IssuePayload.class);
+		TestCycleCaseDefectRelE defectRelE = TestCycleCaseDefectRelEFactory.create();
+		defectRelE.setIssueId(issuePayload.getIssueId());
+		defectRelE.deleteSelf();
 		TestCycleCaseDTO testCycleCaseDTO = new TestCycleCaseDTO();
 		testCycleCaseDTO.setIssueId(issuePayload.getIssueId());
-		testCycleCaseService.batchDelete(testCycleCaseDTO,issuePayload.getProjectId());
+		testCycleCaseService.batchDelete(testCycleCaseDTO, issuePayload.getProjectId());
 
 		TestCaseStepDTO testCaseStepDTO = new TestCaseStepDTO();
 		testCaseStepDTO.setIssueId(issuePayload.getIssueId());
 		testCaseStepService.removeStep(testCaseStepDTO);
-    }
+		return issuePayload;
+	}
 }
