5f060f04f06d9f2499da6981d758d686cc865f73
Jerome_Zuo <842767365@qq.com>
2018-08-14 09:48:03 +0800

    [IMP]解决冲突

--- src/main/resources/script/db/init_tables.groovy
+++ src/main/resources/script/db/init_tables.groovy
@@ -298,13 +298,4 @@ databaseChangeLog(logicalFilePath: 'script/script/init_tables.groovy.groovy') {
             column(name: 'create_time', type: 'BIGINT UNSIGNED', remarks: '创建时间戳')
         }
     }
-
-    changeSet(id: '2018-08-13-add-table-saga_task_instance_record', author: 'jialong.zuo') {
-        createTable(tableName: "saga_task_instance_record") {
-            column(name: 'id', type: 'BIGINT UNSIGNED', autoIncrement: false, remarks: '消息id') {
-                constraints(primaryKey: true)
-            }
-            column(name: 'create_time', type: 'BIGINT UNSIGNED', remarks: '创建时间戳')
-        }
-    }
 }
