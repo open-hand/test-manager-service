17bc9aaa5bd0bfde91b59245ffa15a98d5d1831e
Jerome_Zuo <842767365@qq.com>
2018-08-22 22:32:57 +0800

    [add] 单元测试

---
+++ src/test/groovy/io/choerodon/test/manager/app/service/impl/TestCycleCaseAttachmentRelServiceImplSpec.groovy
@@ -0,0 +1,19 @@
+package io.choerodon.test.manager.app.service.impl
+
+import spock.lang.Specification
+
+/**
+ * Created by jialongZuo@hand-china.com on 8/22/18.
+ */
+
+class TestCycleCaseAttachmentRelServiceImplSpec extends Specification {
+
+    def "Delete"() {
+    }
+
+    def "Upload"() {
+    }
+
+    def "Delete1"() {
+    }
+}
