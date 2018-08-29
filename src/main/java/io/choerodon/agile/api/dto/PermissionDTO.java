e456eb9ac8593cb76cab5c6c2ae4e675df72b8c2
Jerome <jialong.zuo@hand-china.com>
2018-08-29 15:01:35 +0800

    代码风格修改

--- src/main/java/io/choerodon/agile/api/dto/PermissionDTO.java
+++ src/main/java/io/choerodon/agile/api/dto/PermissionDTO.java
@@ -112,42 +112,4 @@ public class PermissionDTO {
 	public void setObjectVersionNumber(Long objectVersionNumber) {
 		this.objectVersionNumber = objectVersionNumber;
 	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		PermissionDTO that = (PermissionDTO) o;
-
-		if (id != null ? !id.equals(that.id) : that.id != null) return false;
-		if (code != null ? !code.equals(that.code) : that.code != null) return false;
-		if (path != null ? !path.equals(that.path) : that.path != null) return false;
-		if (method != null ? !method.equals(that.method) : that.method != null) return false;
-		if (level != null ? !level.equals(that.level) : that.level != null) return false;
-		if (description != null ? !description.equals(that.description) : that.description != null) return false;
-		if (action != null ? !action.equals(that.action) : that.action != null) return false;
-		if (resource != null ? !resource.equals(that.resource) : that.resource != null) return false;
-		if (publicAccess != null ? !publicAccess.equals(that.publicAccess) : that.publicAccess != null) return false;
-		if (loginAccess != null ? !loginAccess.equals(that.loginAccess) : that.loginAccess != null) return false;
-		if (serviceName != null ? !serviceName.equals(that.serviceName) : that.serviceName != null) return false;
-		return objectVersionNumber != null ? objectVersionNumber.equals(that.objectVersionNumber) : that.objectVersionNumber == null;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + (code != null ? code.hashCode() : 0);
-		result = 31 * result + (path != null ? path.hashCode() : 0);
-		result = 31 * result + (method != null ? method.hashCode() : 0);
-		result = 31 * result + (level != null ? level.hashCode() : 0);
-		result = 31 * result + (description != null ? description.hashCode() : 0);
-		result = 31 * result + (action != null ? action.hashCode() : 0);
-		result = 31 * result + (resource != null ? resource.hashCode() : 0);
-		result = 31 * result + (publicAccess != null ? publicAccess.hashCode() : 0);
-		result = 31 * result + (loginAccess != null ? loginAccess.hashCode() : 0);
-		result = 31 * result + (serviceName != null ? serviceName.hashCode() : 0);
-		result = 31 * result + (objectVersionNumber != null ? objectVersionNumber.hashCode() : 0);
-		return result;
-	}
 }
