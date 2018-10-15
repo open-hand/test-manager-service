b367f7cb9f06a38ecf86c50789e93c5f7ef408e2
李宗蔚 <zongwei.li@hand-china.com>
2018-10-15 17:45:09 +0800

    [IMP]

---
+++ src/main/java/io/choerodon/test/manager/domain/service/impl/ITestCaseExcelServiceImpl.java
@@ -0,0 +1,15 @@
+package io.choerodon.test.manager.domain.service.impl;
+
+import io.choerodon.test.manager.api.dto.*;
+import io.choerodon.test.manager.domain.service.IExcelService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by zongw.lee@gmail.com on 15/10/2018
+ */
+public class ITestCaseExcelServiceImpl implements IExcelService<TestIssueFolderDTO,TestCaseStepDTO> {
+    Log log = LogFactory.getLog(this.getClass());
+
+
+}
