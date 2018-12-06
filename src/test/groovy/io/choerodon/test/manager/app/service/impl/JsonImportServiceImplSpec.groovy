7b1879daa8888e9d494478627ef1fc7c36107082
潘浩然 <haoran.pan@hand-china.com>
2018-12-06 16:31:21 +0800

    [IMP]完善测试报告导入接口单测，[FIX]更新automation history的cycle_id和result_id

--- src/test/groovy/io/choerodon/test/manager/app/service/impl/JsonImportServiceImplSpec.groovy
+++ src/test/groovy/io/choerodon/test/manager/app/service/impl/JsonImportServiceImplSpec.groovy
@@ -1,6 +1,5 @@
 package io.choerodon.test.manager.app.service.impl
 
-import feign.FeignException
 import io.choerodon.agile.api.dto.IssueCreateDTO
 import io.choerodon.agile.api.dto.IssueDTO
 import io.choerodon.agile.api.dto.IssueTypeDTO
@@ -10,18 +9,22 @@ import io.choerodon.core.exception.CommonException
 import io.choerodon.devops.api.dto.ApplicationRepDTO
 import io.choerodon.devops.api.dto.ApplicationVersionRepDTO
 import io.choerodon.test.manager.IntegrationTestConfiguration
-import io.choerodon.test.manager.app.service.JsonImportService
 import io.choerodon.test.manager.app.service.TestCaseService
-import io.choerodon.test.manager.domain.service.IExcelImportService
-import io.choerodon.test.manager.domain.service.IJsonImportService
 import io.choerodon.test.manager.domain.service.impl.IExcelImportServiceImpl
 import io.choerodon.test.manager.domain.service.impl.IJsonImportServiceImpl
 import io.choerodon.test.manager.domain.test.manager.entity.TestAppInstanceE
+import io.choerodon.test.manager.domain.test.manager.entity.TestAutomationHistoryE
 import io.choerodon.test.manager.infra.common.utils.FileUtil
+import io.choerodon.test.manager.infra.dataobject.TestCycleCaseDO
+import io.choerodon.test.manager.infra.dataobject.TestIssueFolderRelDO
 import io.choerodon.test.manager.infra.feign.ApplicationFeignClient
 import io.choerodon.test.manager.infra.feign.IssueFeignClient
 import io.choerodon.test.manager.infra.feign.ProjectFeignClient
 import io.choerodon.test.manager.infra.mapper.TestAppInstanceMapper
+import io.choerodon.test.manager.infra.mapper.TestAutomationHistoryMapper
+import io.choerodon.test.manager.infra.mapper.TestAutomationResultMapper
+import io.choerodon.test.manager.infra.mapper.TestCycleCaseMapper
+import io.choerodon.test.manager.infra.mapper.TestIssueFolderRelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
@@ -54,6 +57,18 @@ class JsonImportServiceImplSpec extends Specification {
     @Autowired
     private IExcelImportServiceImpl iExcelImportService
 
+    @Autowired
+    private TestIssueFolderRelMapper issueFolderRelMapper
+
+    @Autowired
+    private TestAutomationResultMapper automationResultMapper
+
+    @Autowired
+    private TestAutomationHistoryMapper automationHistoryMapper
+
+    @Autowired
+    private TestCycleCaseMapper cycleCaseMapper
+
     @Shared
     private String report = new String(FileUtil.unTarGzToMemory(new ClassPathResource("mochawesome.json.tar.gz")
             .file.newInputStream()).get(0), StandardCharsets.UTF_8)
@@ -61,34 +76,49 @@ class JsonImportServiceImplSpec extends Specification {
     @Shared
     private List<TestAppInstanceE> instances = []
 
+    private List<TestAutomationHistoryE> automationHistories = []
+
     def "importMochaReport"() {
         given:
         TestAppInstanceE instanceE = new TestAppInstanceE(
                 code: "mocha-test",
                 projectVersionId: 233L,
                 projectId: 144L,
-                createdBy: 8956L,
-                lastUpdatedBy: 8956L
+                appId: 662,
+                appVersionId: 582L
         )
         instances << instanceE
         instanceMapper.insert(instanceE)
+
+        TestAutomationHistoryE automationHistory = new TestAutomationHistoryE(instanceId: 5L)
+        automationHistories << automationHistory
+        automationHistoryMapper.insert(automationHistory)
+
         ProjectFeignClient projectFeignClient = Mock() {
             _ * query(instanceE.projectId) >>
                     new ResponseEntity<>(new ProjectDTO(organizationId: 1L), HttpStatus.OK)
         }
         ApplicationFeignClient applicationFeignClient = Mock()
+
         iJsonImportService.setProjectFeignClient(projectFeignClient)
         iJsonImportService.setApplicationFeignClient(applicationFeignClient)
+
         IssueFeignClient issueFeignClient = Mock() {
             _ * queryPriorityId(instanceE.projectId, _ as Long) >>
                     new ResponseEntity<>([new PriorityDTO(id: 8L, isDefault: true)], HttpStatus.OK)
             _ * queryIssueType(instanceE.projectId, "test", _ as Long) >>
                     new ResponseEntity<>([new IssueTypeDTO(typeCode: "issue_test", id: 18L)], HttpStatus.OK)
         }
+
         iExcelImportService.setIssueFeignClient(issueFeignClient)
-        TestCaseService testCaseService = Mock() {
-            _ * createTest(_ as IssueCreateDTO, instanceE.projectId, "test") >> new IssueDTO(issueId: 1L)
+        def issueDTOs = []
+        for (long i in 1000001L..1000009L) {
+            issueDTOs << new IssueDTO(issueId: i)
         }
+        TestCaseService testCaseService = Mock() {
+            _ * createTest(_ as IssueCreateDTO, instanceE.projectId, "test") >>> issueDTOs
+        }
+
         iExcelImportService.setTestCaseService(testCaseService)
 
         when: "app instance 不存在"
@@ -117,13 +147,18 @@ class JsonImportServiceImplSpec extends Specification {
         completionException = thrown()
         completionException.cause.message == "error.get.app.name"
 
-//        when:
-//        Long reportId = jsonImportService.importMochaReport("att-662-582-5", report)
-//        then:
-//        1 * applicationFeignClient.getAppversion(instanceE.projectId, _ as Long) >>
-//                new ResponseEntity<>(new ApplicationVersionRepDTO(version: "测试版本号"), HttpStatus.OK)
-//        1 * applicationFeignClient.queryByAppId(instanceE.projectId, _ as Long) >>
-//                new ResponseEntity<>(new ApplicationRepDTO(name: "应用名称"), HttpStatus.OK)
-//        reportId == 1L
+        when:
+        Long reportId = jsonImportService.importMochaReport("att-662-582-5", report)
+        for (long i in 1000000L..1000009L) {
+            issueFolderRelMapper.delete(new TestIssueFolderRelDO(issueId: i))
+            cycleCaseMapper.delete(new TestCycleCaseDO(issueId: i))
+        }
+        automationResultMapper.deleteByPrimaryKey(reportId)
+        then:
+        1 * applicationFeignClient.getAppversion(instanceE.projectId, _ as List<Long>) >>
+                new ResponseEntity<>([new ApplicationVersionRepDTO(version: "测试版本号")], HttpStatus.OK)
+        1 * applicationFeignClient.queryByAppId(instanceE.projectId, _ as Long) >>
+                new ResponseEntity<>(new ApplicationRepDTO(name: "应用名称"), HttpStatus.OK)
+        reportId == 2L
     }
 }
