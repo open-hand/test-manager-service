af2200a9c94817e2736bdfb8a0b8a219d2727aef
Jerome <jialong.zuo@hand-china.com>
2018-12-12 17:00:50 +0800

    移除powerMock

--- src/test/groovy/io/choerodon/test/manager/app/service/impl/SecondExcelServiceImplSpec.groovy
+++ src/test/groovy/io/choerodon/test/manager/app/service/impl/SecondExcelServiceImplSpec.groovy
@@ -1,62 +1,62 @@
-package io.choerodon.test.manager.app.service.impl
-
-
-import org.junit.runner.RunWith
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.PowerMockRunner
-import org.powermock.modules.junit4.PowerMockRunnerDelegate
-import org.spockframework.runtime.Sputnik
-import org.springframework.aop.framework.AopContext
-import spock.lang.Specification
-
-@RunWith(PowerMockRunner.class)
-@PowerMockRunnerDelegate(Sputnik.class)
-@PrepareForTest([AopContext.class])
-class SecondExcelServiceImplSpec extends Specification {
-
-//    @Autowired
-//    ExcelServiceHandler excelServiceHandler
+//package io.choerodon.test.manager.app.service.impl
 //
-//    ExcelService excelService = Mock(ExcelService)
-
-//    def setup() {
-        //mock静态方法-CustomUserDetails
-//        PowerMockito.mockStatic(AopContext)
-//        PowerMockito.when(AopContext.currentProxy()).thenReturn(excelService)
-//    }
-
-//    def "ExportCycleCaseInOneCycle"() {
-//        when:
-//        excelServiceHandler.exportCycleCaseInOneCycle(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
-//        then:
-//        1 * excelService.exportCycleCaseInOneCycleByTransaction(_, _, _, _, _, _)
-//    }
 //
-//    def "ExportCaseByProject"() {
-//        when:
-//        excelServiceHandler.exportCaseByProject(1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
-//        then:
-//        1 * excelService.exportCaseProjectByTransaction(_, _, _, _, _)
-//    }
+//import org.junit.runner.RunWith
+//import org.powermock.core.classloader.annotations.PrepareForTest
+//import org.powermock.modules.junit4.PowerMockRunner
+//import org.powermock.modules.junit4.PowerMockRunnerDelegate
+//import org.spockframework.runtime.Sputnik
+//import org.springframework.aop.framework.AopContext
+//import spock.lang.Specification
 //
-//    def "ExportCaseByVersion"() {
-//        when:
-//        excelServiceHandler.exportCaseByVersion(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
-//        then:
-//        1 * excelService.exportCaseVersionByTransaction(_, _, _, _, _, _)
-//    }
+//@RunWith(PowerMockRunner.class)
+//@PowerMockRunnerDelegate(Sputnik.class)
+//@PrepareForTest([AopContext.class])
+//class SecondExcelServiceImplSpec extends Specification {
 //
-//    def "ExportCaseByFolder"() {
-//        when:
-//        excelServiceHandler.exportCaseByFolder(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
-//        then:
-//        1 * excelService.exportCaseFolderByTransaction(_, _, _, _, _, _)
-//    }
+////    @Autowired
+////    ExcelServiceHandler excelServiceHandler
+////
+////    ExcelService excelService = Mock(ExcelService)
 //
-//    def "exportFailCase"() {
-//        when:
-//        excelServiceHandler.exportFailCase(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
-//        then:
-//        1 * excelService.exportFailCaseByTransaction(_, _, _)
-//    }
-}
+////    def setup() {
+//        //mock静态方法-CustomUserDetails
+////        PowerMockito.mockStatic(AopContext)
+////        PowerMockito.when(AopContext.currentProxy()).thenReturn(excelService)
+////    }
+//
+////    def "ExportCycleCaseInOneCycle"() {
+////        when:
+////        excelServiceHandler.exportCycleCaseInOneCycle(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
+////        then:
+////        1 * excelService.exportCycleCaseInOneCycleByTransaction(_, _, _, _, _, _)
+////    }
+////
+////    def "ExportCaseByProject"() {
+////        when:
+////        excelServiceHandler.exportCaseByProject(1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
+////        then:
+////        1 * excelService.exportCaseProjectByTransaction(_, _, _, _, _)
+////    }
+////
+////    def "ExportCaseByVersion"() {
+////        when:
+////        excelServiceHandler.exportCaseByVersion(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
+////        then:
+////        1 * excelService.exportCaseVersionByTransaction(_, _, _, _, _, _)
+////    }
+////
+////    def "ExportCaseByFolder"() {
+////        when:
+////        excelServiceHandler.exportCaseByFolder(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
+////        then:
+////        1 * excelService.exportCaseFolderByTransaction(_, _, _, _, _, _)
+////    }
+////
+////    def "exportFailCase"() {
+////        when:
+////        excelServiceHandler.exportFailCase(1L, 1L, new MockHttpServletRequest(), new MockHttpServletResponse(), 1L)
+////        then:
+////        1 * excelService.exportFailCaseByTransaction(_, _, _)
+////    }
+//}
--- src/test/groovy/io/choerodon/test/manager/app/service/impl/TestFileLoadHistoryServiceImplSpec.groovy
+++ src/test/groovy/io/choerodon/test/manager/app/service/impl/TestFileLoadHistoryServiceImplSpec.groovy
@@ -1,59 +1,59 @@
-package io.choerodon.test.manager.app.service.impl
-
-import io.choerodon.core.convertor.ConvertHelper
-import io.choerodon.core.oauth.CustomUserDetails
-import io.choerodon.core.oauth.DetailsHelper
-import io.choerodon.test.manager.api.dto.TestFileLoadHistoryDTO
-import io.choerodon.test.manager.app.service.TestFileLoadHistoryService
-import io.choerodon.test.manager.domain.service.ITestFileLoadHistoryService
-import io.choerodon.test.manager.domain.test.manager.entity.TestFileLoadHistoryE
-import io.choerodon.test.manager.infra.common.utils.SpringUtil
-import org.junit.runner.RunWith
-import org.powermock.api.mockito.PowerMockito
-import org.powermock.core.classloader.annotations.PrepareForTest
-import org.powermock.modules.junit4.PowerMockRunner
-import org.powermock.modules.junit4.PowerMockRunnerDelegate
-import org.spockframework.runtime.Sputnik
-import org.springframework.context.ApplicationContext
-import spock.lang.Specification
-
-@PrepareForTest([DetailsHelper, SpringUtil, ConvertHelper])
-@RunWith(PowerMockRunner)
-@PowerMockRunnerDelegate(Sputnik)
-class TestFileLoadHistoryServiceImplSpec extends Specification {
-
-    def "queryLatestImportIssueHistory"() {
-        given:
-        CustomUserDetails userDetails = new CustomUserDetails("test", "12345678", Collections.emptyList())
-        userDetails.userId = 0L
-
-        TestFileLoadHistoryDTO testFileLoadHistoryDTO = new TestFileLoadHistoryDTO()
-        TestFileLoadHistoryE testFileLoadHistoryE = new TestFileLoadHistoryE()
-
-        ApplicationContext ctx = PowerMockito.mock(ApplicationContext)
-        PowerMockito.when(ctx.getBean(TestFileLoadHistoryE)).thenReturn(testFileLoadHistoryE)
-        PowerMockito.mockStatic(DetailsHelper)
-        PowerMockito.when(DetailsHelper.getUserDetails()).thenReturn(userDetails)
-        PowerMockito.mockStatic(SpringUtil)
-        PowerMockito.when(SpringUtil.getApplicationContext()).thenReturn(ctx)
-        PowerMockito.mockStatic(ConvertHelper)
-        PowerMockito.when(ConvertHelper.convert(testFileLoadHistoryE, TestFileLoadHistoryDTO)).thenReturn(testFileLoadHistoryDTO)
-
-        ITestFileLoadHistoryService iTestFileLoadHistoryService = PowerMockito.mock(ITestFileLoadHistoryService)
-        PowerMockito.when(iTestFileLoadHistoryService.queryLatestImportIssueHistory(testFileLoadHistoryE)).thenReturn(testFileLoadHistoryE)
-
-        TestFileLoadHistoryService loadHistoryService = new TestFileLoadHistoryServiceImpl(iTestFileLoadHistoryService: iTestFileLoadHistoryService)
-
-        when:
-        TestFileLoadHistoryDTO loadHistoryDTO = loadHistoryService.queryLatestImportIssueHistory()
-        then:
-        Objects.equals(loadHistoryDTO, testFileLoadHistoryDTO)
-
-        when:
-        PowerMockito.when(iTestFileLoadHistoryService.queryLatestImportIssueHistory(testFileLoadHistoryE)).thenReturn(null)
-        loadHistoryDTO = loadHistoryService.queryLatestImportIssueHistory()
-        then:
-        loadHistoryDTO == null
-
-    }
-}
+//package io.choerodon.test.manager.app.service.impl
+//
+//import io.choerodon.core.convertor.ConvertHelper
+//import io.choerodon.core.oauth.CustomUserDetails
+//import io.choerodon.core.oauth.DetailsHelper
+//import io.choerodon.test.manager.api.dto.TestFileLoadHistoryDTO
+//import io.choerodon.test.manager.app.service.TestFileLoadHistoryService
+//import io.choerodon.test.manager.domain.service.ITestFileLoadHistoryService
+//import io.choerodon.test.manager.domain.test.manager.entity.TestFileLoadHistoryE
+//import io.choerodon.test.manager.infra.common.utils.SpringUtil
+//import org.junit.runner.RunWith
+//import org.powermock.api.mockito.PowerMockito
+//import org.powermock.core.classloader.annotations.PrepareForTest
+//import org.powermock.modules.junit4.PowerMockRunner
+//import org.powermock.modules.junit4.PowerMockRunnerDelegate
+//import org.spockframework.runtime.Sputnik
+//import org.springframework.context.ApplicationContext
+//import spock.lang.Specification
+//
+//@PrepareForTest([DetailsHelper, SpringUtil, ConvertHelper])
+//@RunWith(PowerMockRunner)
+//@PowerMockRunnerDelegate(Sputnik)
+//class TestFileLoadHistoryServiceImplSpec extends Specification {
+//
+//    def "queryLatestImportIssueHistory"() {
+//        given:
+//        CustomUserDetails userDetails = new CustomUserDetails("test", "12345678", Collections.emptyList())
+//        userDetails.userId = 0L
+//
+//        TestFileLoadHistoryDTO testFileLoadHistoryDTO = new TestFileLoadHistoryDTO()
+//        TestFileLoadHistoryE testFileLoadHistoryE = new TestFileLoadHistoryE()
+//
+//        ApplicationContext ctx = PowerMockito.mock(ApplicationContext)
+//        PowerMockito.when(ctx.getBean(TestFileLoadHistoryE)).thenReturn(testFileLoadHistoryE)
+//        PowerMockito.mockStatic(DetailsHelper)
+//        PowerMockito.when(DetailsHelper.getUserDetails()).thenReturn(userDetails)
+//        PowerMockito.mockStatic(SpringUtil)
+//        PowerMockito.when(SpringUtil.getApplicationContext()).thenReturn(ctx)
+//        PowerMockito.mockStatic(ConvertHelper)
+//        PowerMockito.when(ConvertHelper.convert(testFileLoadHistoryE, TestFileLoadHistoryDTO)).thenReturn(testFileLoadHistoryDTO)
+//
+//        ITestFileLoadHistoryService iTestFileLoadHistoryService = PowerMockito.mock(ITestFileLoadHistoryService)
+//        PowerMockito.when(iTestFileLoadHistoryService.queryLatestImportIssueHistory(testFileLoadHistoryE)).thenReturn(testFileLoadHistoryE)
+//
+//        TestFileLoadHistoryService loadHistoryService = new TestFileLoadHistoryServiceImpl(iTestFileLoadHistoryService: iTestFileLoadHistoryService)
+//
+//        when:
+//        TestFileLoadHistoryDTO loadHistoryDTO = loadHistoryService.queryLatestImportIssueHistory()
+//        then:
+//        Objects.equals(loadHistoryDTO, testFileLoadHistoryDTO)
+//
+//        when:
+//        PowerMockito.when(iTestFileLoadHistoryService.queryLatestImportIssueHistory(testFileLoadHistoryE)).thenReturn(null)
+//        loadHistoryDTO = loadHistoryService.queryLatestImportIssueHistory()
+//        then:
+//        loadHistoryDTO == null
+//
+//    }
+//}
