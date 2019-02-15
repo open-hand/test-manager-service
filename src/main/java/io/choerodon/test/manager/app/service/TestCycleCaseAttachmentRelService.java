425f5f8aeb7c60aef875dc66026e508126de4133
dinghuang <dinghuang123@gmail.com>
2019-02-15 11:49:59 +0800

    [IMP] 上传多个附件修改为在同一个事务中执行

--- src/main/java/io/choerodon/test/manager/app/service/TestCycleCaseAttachmentRelService.java
+++ src/main/java/io/choerodon/test/manager/app/service/TestCycleCaseAttachmentRelService.java
@@ -3,14 +3,28 @@ package io.choerodon.test.manager.app.service;
 import io.choerodon.test.manager.api.dto.TestCycleCaseAttachmentRelDTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 /**
  * Created by 842767365@qq.com on 6/11/18.
  */
 public interface TestCycleCaseAttachmentRelService {
 
-	TestCycleCaseAttachmentRelDTO upload(String bucketName, String fileName, MultipartFile file, Long attachmentLinkId, String attachmentType, String comment);
+    TestCycleCaseAttachmentRelDTO upload(String bucketName, String fileName, MultipartFile file, Long attachmentLinkId, String attachmentType, String comment);
 
     void delete(String bucketName, Long attachId);
 
-	void delete(Long linkedId, String Type);
+    void delete(Long linkedId, String Type);
+
+    /**
+     * 上传多个附件
+     *
+     * @param request          request
+     * @param bucketName       bucketName
+     * @param attachmentLinkId attachmentLinkId
+     * @param attachmentType   attachmentType
+     * @return TestCycleCaseAttachmentRelDTO
+     */
+    List<TestCycleCaseAttachmentRelDTO> uploadMultipartFile(HttpServletRequest request, String bucketName, Long attachmentLinkId, String attachmentType);
 }
--- src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleCaseAttachmentRelServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleCaseAttachmentRelServiceImpl.java
@@ -11,7 +11,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -22,6 +26,9 @@ public class TestCycleCaseAttachmentRelServiceImpl implements TestCycleCaseAttac
 
     @Autowired
     ITestCycleCaseAttachmentRelService iTestCycleCaseAttachmentRelService;
+
+    @Autowired
+    TestCycleCaseAttachmentRelService testCycleCaseAttachmentRelService;
 
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -44,8 +51,17 @@ public class TestCycleCaseAttachmentRelServiceImpl implements TestCycleCaseAttac
         attachmentRelE.setAttachmentLinkId(linkedId);
         attachmentRelE.setAttachmentType(type);
         Optional.ofNullable(attachmentRelE.querySelf()).ifPresent(m ->
-            m.forEach(v -> iTestCycleCaseAttachmentRelService
-                    .delete(TestCycleCaseAttachmentRelE.ATTACHMENT_BUCKET, v.getId()))
+                m.forEach(v -> iTestCycleCaseAttachmentRelService
+                        .delete(TestCycleCaseAttachmentRelE.ATTACHMENT_BUCKET, v.getId()))
         );
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public List<TestCycleCaseAttachmentRelDTO> uploadMultipartFile(HttpServletRequest request, String bucketName, Long attachmentLinkId, String attachmentType) {
+        List<TestCycleCaseAttachmentRelDTO> testCycleCaseAttachmentRelDTOS = new ArrayList<>();
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        files.forEach(v -> testCycleCaseAttachmentRelDTOS.add(testCycleCaseAttachmentRelService.upload(bucketName, v.getOriginalFilename(), v, attachmentLinkId, attachmentType, null)));
+        return testCycleCaseAttachmentRelDTOS;
     }
 }
