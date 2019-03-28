65727b7f6bfc0778ace87006c161de1ea12d7378
王喆 <ettwz@hotmail.com>
2019-03-28 12:10:14 +0800

    [IMP]修改单测

--- src/test/groovy/io/choerodon/test/manager/app/service/impl/UserServiceImplSpec.groovy
+++ src/test/groovy/io/choerodon/test/manager/app/service/impl/UserServiceImplSpec.groovy
@@ -20,93 +20,93 @@ class UserServiceImplSpec extends Specification {
     UserServiceImpl userService;
     UserFeignClient client;
 
-    def setup(){
-        client=Mock(UserFeignClient)
-        userService=new UserServiceImpl(userFeignClient:client)
+    def setup() {
+        client = Mock(UserFeignClient)
+        userService = new UserServiceImpl(userFeignClient: client)
     }
 
     def "Query"() {
         given:
-        UserDO user=new UserDO(id:1L)
+        UserDO user = new UserDO(id: 1L)
         when:
-        userService.query([1,2,3] as Long[])
+        userService.query([1, 2, 3] as Long[])
         then:
-        1*client.listUsersByIds(_)>>new ResponseEntity<>(Lists.newArrayList(user),HttpStatus.OK)
+        1 * client.listUsersByIds(_, false) >> new ResponseEntity<>(Lists.newArrayList(user), HttpStatus.OK)
         when:
         userService.query([] as Long[])
         then:
-        0*client.listUsersByIds(_)
+        0 * client.listUsersByIds(_, false)
     }
 
     def "List"() {
         given:
         PageRequest pr = new PageRequest()
-        UserDTO user=new UserDTO(id:1L)
-        PageInfo info=new PageInfo(1,1)
+        UserDTO user = new UserDTO(id: 1L)
+        PageInfo info = new PageInfo(1, 1)
 
-        Page<UserDTO> page1 = new Page<>(Lists.newArrayList(user),info,1)
+        Page<UserDTO> page1 = new Page<>(Lists.newArrayList(user), info, 1)
 
         when:
-        userService.list(pr,1l,"参数",2l)
+        userService.list(pr, 1l, "参数", 2l)
         then:
-        1*client.list(_,_,_,_,_)>>new ResponseEntity<>(page1,HttpStatus.OK)
+        1 * client.list(_, _, _, _, _) >> new ResponseEntity<>(page1, HttpStatus.OK)
     }
 
-    def "populateUsersInHistory"(){
-        TestCycleCaseHistoryDTO dto=new TestCycleCaseHistoryDTO(lastUpdatedBy: 111L);
-        TestCycleCaseHistoryDTO dto1=new TestCycleCaseHistoryDTO(lastUpdatedBy: 0L);
-        List<TestCycleCaseHistoryDTO> dtos=Lists.newArrayList(dto)
+    def "populateUsersInHistory"() {
+        TestCycleCaseHistoryDTO dto = new TestCycleCaseHistoryDTO(lastUpdatedBy: 111L);
+        TestCycleCaseHistoryDTO dto1 = new TestCycleCaseHistoryDTO(lastUpdatedBy: 0L);
+        List<TestCycleCaseHistoryDTO> dtos = Lists.newArrayList(dto)
         dtos.add(dto1)
         when:
         userService.populateUsersInHistory(dtos)
         then:
-        1*client.listUsersByIds(_)>> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)),HttpStatus.OK)
+        1 * client.listUsersByIds(_, false) >> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)), HttpStatus.OK)
         when:
         userService.populateUsersInHistory(new ArrayList<TestCycleCaseHistoryDTO>())
         then:
-        0*client.listUsersByIds(_)
+        0 * client.listUsersByIds(_, false)
         when:
         userService.populateUsersInHistory(Lists.newArrayList(new TestCycleCaseHistoryDTO(lastUpdatedBy: 111L)))
         then:
-        1*client.listUsersByIds(_)>> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)),HttpStatus.OK)
+        1 * client.listUsersByIds(_, false) >> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)), HttpStatus.OK)
     }
 
-    def "populateTestCycleCaseDTO"(){
-        TestCycleCaseDTO dto=new TestCycleCaseDTO(lastUpdatedBy: 111L,assignedTo: 0L);
+    def "populateTestCycleCaseDTO"() {
+        TestCycleCaseDTO dto = new TestCycleCaseDTO(lastUpdatedBy: 111L, assignedTo: 0L);
         when:
         userService.populateTestCycleCaseDTO(dto)
         then:
-        1*client.listUsersByIds(_)>> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)),HttpStatus.OK)
+        1 * client.listUsersByIds(_, false) >> new ResponseEntity(Lists.newArrayList(new UserDO(id: 111L)), HttpStatus.OK)
         when:
-        userService.populateTestCycleCaseDTO(new TestCycleCaseDTO(lastUpdatedBy:0L))
+        userService.populateTestCycleCaseDTO(new TestCycleCaseDTO(lastUpdatedBy: 0L))
         then:
-        0*client.listUsersByIds(_)
+        0 * client.listUsersByIds(_, false)
     }
 
-    def "populateTestAutomationHistory"(){
+    def "populateTestAutomationHistory"() {
         given:
-        Page page=new Page()
+        Page page = new Page()
         page.setContent(Lists.newArrayList(new TestAutomationHistoryDTO(createdBy: 11L)))
         when:
         userService.populateTestAutomationHistory(page);
         then:
-        1*client.listUsersByIds(_)>> new ResponseEntity(Lists.newArrayList(new UserDO(id: 11)),HttpStatus.OK)
+        1 * client.listUsersByIds(_, false) >> new ResponseEntity(Lists.newArrayList(new UserDO(id: 11)), HttpStatus.OK)
         when:
         page.setContent([])
         userService.populateTestAutomationHistory(page);
         then:
-        0*client.listUsersByIds(_)
+        0 * client.listUsersByIds(_, false)
 
 
     }
 
-    def "isUserId"(){
+    def "isUserId"() {
         expect:
-        LongUtils.isUserId(param1)==result
+        LongUtils.isUserId(param1) == result
         where:
-        param1  |   result
-        0L      |   false
-        1L      |   true
-        null    |   false
+        param1 | result
+        0L     | false
+        1L     | true
+        null   | false
     }
 }
