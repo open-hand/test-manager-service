649cb0e265b1a5c2778e7c39bc76fff35c296b11
王喆 <ettwz@hotmail.com>
2019-06-21 14:52:18 +0800

    [IMP]解决冲突

--- react/config.js
+++ react/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // server: 'http://api.alpha.saas.hand-china.com',
-  server: 'http://10.211.108.232:8080', // 王喆
+  server: 'http://api.alpha.saas.hand-china.com',
+  // server: 'http://10.211.108.232:8080', // 王喆
   master: '@choerodon/master',
   projectType: 'choerodon',
   buildType: 'single',
