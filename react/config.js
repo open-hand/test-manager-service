f0fa7ded31bf98808bc795c2cd2285a4fb3a3d0c
WangZhe <wangzhe@WangZhes-iMac.local>
2019-07-11 16:58:12 +0800

    [IMP]修改测试步骤读取列索引错误

--- react/config.js
+++ react/config.js
@@ -1,11 +1,11 @@
 const config = {
-  server: 'http://api.alpha.saas.hand-china.com',
-  // server: 'http://10.211.111.134:8080', // 王喆
+  // server: 'http://api.alpha.saas.hand-china.com',
+  server: 'http://10.211.111.134:8080', // 王喆
   master: '@choerodon/master',
   projectType: 'choerodon',
   buildType: 'single',
   dashboard: {},
-  // webSocketServer: 'ws://10.211.111.134:18085',
+  webSocketServer: 'ws://10.211.111.134:18085',
 };
 
 module.exports = config;
