acdda200bfe1eed319e6fa958feb29aa4933ca5b
laincarl <1047972853@qq.com>
2019-08-02 16:14:48 +0800

    [FIX]测试用例侧边样式更改

--- react/config.js
+++ react/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // server: 'http://api.staging.saas.hand-china.com',
-  server: 'http://api.alpha.saas.hand-china.com',
+  server: 'http://api.staging.saas.hand-china.com',
+  // server: 'http://api.alpha.saas.hand-china.com',
   // server: 'http://10.211.111.134:8080', // 王喆
   master: '@choerodon/master',
   projectType: 'choerodon',
