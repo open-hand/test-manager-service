c36f3a690aeef13c81fe120fdf6564f8eb26c40b
陈士男 <shinan.chen@hand-china.com>
2019-09-27 14:38:15 +0800

    [UPD] 调整testStatus接口权限

--- src/main/java/io/choerodon/test/manager/api/controller/v1/TestStatusController.java
+++ src/main/java/io/choerodon/test/manager/api/controller/v1/TestStatusController.java
@@ -1,20 +1,19 @@
 package io.choerodon.test.manager.api.controller.v1;
 
-import java.util.List;
-import java.util.Optional;
-
+import io.choerodon.base.annotation.Permission;
+import io.choerodon.base.enums.ResourceType;
+import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.iam.InitRoleCode;
+import io.choerodon.test.manager.api.vo.TestStatusVO;
+import io.choerodon.test.manager.app.service.TestStatusService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import io.choerodon.base.enums.ResourceType;
-import io.choerodon.core.iam.InitRoleCode;
-import io.choerodon.base.annotation.Permission;
-import io.choerodon.test.manager.api.vo.TestStatusVO;
-import io.choerodon.test.manager.app.service.TestStatusService;
-import io.choerodon.core.exception.CommonException;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by 842767365@qq.com on 6/25/18.
@@ -37,7 +36,17 @@ public class TestStatusController {
 
     }
 
-    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_MEMBER, InitRoleCode.PROJECT_OWNER})
+    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_OWNER})
+    @ApiOperation("更新状态")
+    @PutMapping("/update")
+    public ResponseEntity<TestStatusVO> update(@PathVariable(name = "project_id") Long projectId,
+                                               @RequestBody TestStatusVO testStatusVO) {
+        return Optional.ofNullable(testStatusService.update(testStatusVO))
+                .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
+                .orElseThrow(() -> new CommonException("error.testStatus.update"));
+    }
+
+    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_OWNER})
     @ApiOperation("删除状态")
     @DeleteMapping("/{statusId}")
     public ResponseEntity delete(@PathVariable(name = "project_id") Long projectId,
@@ -48,7 +57,7 @@ public class TestStatusController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_MEMBER, InitRoleCode.PROJECT_OWNER})
+    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_OWNER})
     @ApiOperation("插入状态")
     @PostMapping
     public ResponseEntity<TestStatusVO> insert(@PathVariable(name = "project_id") Long projectId,
@@ -56,15 +65,5 @@ public class TestStatusController {
         return Optional.ofNullable(testStatusService.insert(testStatusVO))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
                 .orElseThrow(() -> new CommonException("error.testStatus.insert"));
-    }
-
-    @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_MEMBER, InitRoleCode.PROJECT_OWNER})
-    @ApiOperation("更新状态")
-    @PutMapping("/update")
-    public ResponseEntity<TestStatusVO> update(@PathVariable(name = "project_id") Long projectId,
-                                               @RequestBody TestStatusVO testStatusVO) {
-        return Optional.ofNullable(testStatusService.update(testStatusVO))
-                .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
-                .orElseThrow(() -> new CommonException("error.testStatus.update"));
     }
 }
