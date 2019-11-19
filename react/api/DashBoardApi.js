d8379cad144641a538981648fc521ed4a4e75e5b
laincarl <1047972853@qq.com>
2019-11-19 15:20:32 +0800

    [DEL]删除无用文件

--- react/api/DashBoardApi.js
+++
@@ -1,5 +0,0 @@
-import { getProjectId, request } from '../common/utils';
-
-export default function loadProgressByVersion(versionId, cycleId) {
-  return request.get(`/test/v1/projects/${getProjectId()}/cycle/count/color/in/version/${versionId}${cycleId ? `?cycleId=${cycleId}` : ''}`);
-}
--- react/components/SimpleSelect/index.js
+++
@@ -1,3 +0,0 @@
-import SimpleSelect from './SimpleSelect';
-
-export default SimpleSelect;
