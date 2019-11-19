22a57ad568bbc2350708e14dabf622ef5ff429e4
laincarl <1047972853@qq.com>
2019-11-19 11:59:04 +0800

    [ADD]默认选中第一个文件夹

--- react/routes/IssueManage/components/Tree/TreeNode.js
+++ react/routes/IssueManage/components/Tree/TreeNode.js
@@ -83,7 +83,7 @@ export default function TreeNode(props) {
       role="none"
       className={`${prefix}-tree-item`}
     >
-      <TextField style={{ marginLeft: 20 }} defaultValue={item.data.name} onBlur={onSave} autoFocus />
+      <TextField style={{ marginLeft: 38 }} defaultValue={item.data.name} autoFocus />
     </div>
   );
   const renderTitle = () => {
