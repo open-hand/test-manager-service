bac0e67bf5ac6494d424a2ea8cf81baad34f10c8
lizhaozhong <zhaozhong.li@hand-china.com>
2019-11-21 20:06:12 +0800

    [IMP] 更新数据库迁移接口

--- src/main/java/io/choerodon/test/manager/infra/mapper/TestCaseLabelMapper.java
+++ src/main/java/io/choerodon/test/manager/infra/mapper/TestCaseLabelMapper.java
@@ -3,8 +3,8 @@ package io.choerodon.test.manager.infra.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-import tk.mybatis.mapper.common.BaseMapper;
 
+import io.choerodon.mybatis.common.Mapper;
 import io.choerodon.test.manager.infra.dto.TestCaseLabelDTO;
 
 /**
@@ -12,7 +12,7 @@ import io.choerodon.test.manager.infra.dto.TestCaseLabelDTO;
  * @date: 2019/11/20 14:52
  * @description:
  */
-public interface TestCaseLabelMapper extends BaseMapper<TestCaseLabelDTO> {
+public interface TestCaseLabelMapper extends Mapper<TestCaseLabelDTO> {
     void batchInsert(List<TestCaseLabelDTO> testCaseLabelDTOList);
 
     List<TestCaseLabelDTO> listByIds(@Param("labelIds") List<Long> labelIds);
