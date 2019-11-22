bb20337486cbe074c39e77e0e5834d018e660f3f
lizhaozhong <zhaozhong.li@hand-china.com>
2019-11-22 11:03:41 +0800

    [IMP] 修复label数据迁移feign接口

--- src/main/java/io/choerodon/test/manager/infra/feign/TestIssueLabelRelFeignClient.java
+++ src/main/java/io/choerodon/test/manager/infra/feign/TestIssueLabelRelFeignClient.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import io.choerodon.agile.api.vo.LabelIssueRelFixVO;
@@ -22,7 +23,7 @@ import io.choerodon.test.manager.infra.feign.callback.TestIssueLabelRelFeignClie
 @FeignClient(value = "agile-service", fallback = TestIssueLabelRelFeignClientFallback.class)
 public interface TestIssueLabelRelFeignClient {
 
-    @GetMapping(value = "/v1/projects/{project_id}/issue_labels_rel/query")
+    @PostMapping(value = "/v1/projects/{project_id}/issue_labels_rel/query")
     ResponseEntity<List<LabelIssueRelFixVO>> queryIssueLabelRelList(@ApiParam(value = "项目id", required = true)
                                                                     @PathVariable(name = "project_id") Long projectId,
                                                                     @ApiParam(value = "issueIds", required = true)
