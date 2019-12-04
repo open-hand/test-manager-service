57b044d41d750cf45f3a3c09ecff6465c6f91c97
zhaotianxin <ztxemail@163.com>
2019-12-04 12:34:57 +0800

    [DEL] 删除CompareVO

--- src/main/java/io/choerodon/test/manager/api/vo/CaseStepVO.java
+++
@@ -1,9 +0,0 @@
-package io.choerodon.test.manager.api.vo;
-
-/**
- * @author zhaotianxin
- * @since 2019/12/4
- */
-public class CaseStepVO {
-    private String rank;
-}
--- src/main/java/io/choerodon/test/manager/api/vo/CaseVO.java
+++
@@ -1,8 +0,0 @@
-package io.choerodon.test.manager.api.vo;
-
-/**
- * @author zhaotianxin
- * @since 2019/12/4
- */
-public class CaseVO {
-}
