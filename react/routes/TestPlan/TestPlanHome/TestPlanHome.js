476c84396967d669358490b195e0360116b38c91
812913 <lwfvirgo@163.com>
2019-12-09 10:07:29 +0800

    [FIX] 修复完成计划和开始进行计划时tab页不切换的问题

--- react/routes/TestPlan/TestPlanHome/TestPlanHome.js
+++ react/routes/TestPlan/TestPlanHome/TestPlanHome.js
@@ -245,7 +245,7 @@ function TestPlanHome() {
         <div className={`${prefixCls}-contentWrap`}>
           <div className={`${prefixCls}-contentWrap-left`}>
             <div className={`${prefixCls}-contentWrap-testPlanTree`}>
-              <Tabs defaultActiveKey="todo" onChange={handleTabsChange} value={testPlanStatus}>
+              <Tabs defaultActiveKey="todo" onChange={handleTabsChange} activeKey={testPlanStatus}>
                 <TabPane tab="未开始" key="todo">
                   <TestPlanTree />
                 </TabPane>
