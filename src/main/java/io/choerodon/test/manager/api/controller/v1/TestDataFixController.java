cc2d6b8f440433f48c2c978cb7f49327d73624cd
HuangFuqiang <fuqianghuang01@gmail.com>
2019-12-13 11:47:43 +0800

    [FIX]删除多余接口

--- src/main/java/io/choerodon/test/manager/api/controller/v1/TestDataFixController.java
+++ src/main/java/io/choerodon/test/manager/api/controller/v1/TestDataFixController.java
@@ -1,8 +1,5 @@
 package io.choerodon.test.manager.api.controller.v1;
 
-import io.choerodon.test.manager.api.vo.DemoPayload;
-import io.choerodon.test.manager.app.eventhandler.DemoDataEventHandler;
-import io.choerodon.test.manager.app.service.DemoService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,23 +23,11 @@ public class TestDataFixController {
     @Autowired
     private DataMigrationService dataMigrationService;
 
-    @Autowired
-    private DemoService demoService;
-
     @Permission(type = ResourceType.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
     @ApiOperation("迁移数据")
     @GetMapping
     public ResponseEntity fix() {
         dataMigrationService.fixData();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-
-    @Permission(type = ResourceType.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
-    @ApiOperation("demo")
-    @PostMapping("/demo")
-    public ResponseEntity demoInit(@RequestBody DemoPayload demoPayload) {
-        demoService.demoInit(demoPayload);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
