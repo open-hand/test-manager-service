398e90e94261bfc26251f38ac6fc7f91869844d6
HuangFuqiang <fuqianghuang01@gmail.com>
2019-12-15 16:43:41 +0800

    [ADD]添加迁移数据定时任务

---
+++ src/main/java/io/choerodon/test/manager/infra/task/FixDataTask.java
@@ -0,0 +1,24 @@
+package io.choerodon.test.manager.infra.task;
+
+import io.choerodon.asgard.saga.dto.StartInstanceDTO;
+import io.choerodon.asgard.schedule.QuartzDefinition;
+import io.choerodon.asgard.schedule.annotation.JobTask;
+import io.choerodon.asgard.schedule.annotation.TimedTask;
+import io.choerodon.test.manager.app.service.DataMigrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class FixDataTask {
+
+    @Autowired
+    private DataMigrationService dataMigrationService;
+
+    @JobTask(maxRetryCount = 3, code = "syncAgileToTestManager", description = "升级到0.20.0,同步敏捷数据到测试管理服务")
+    @TimedTask(name = "syncAgileToTestManager", description = "升级到0.20.0,同步敏捷数据到测试管理服务", oneExecution = true, repeatCount = 0, repeatInterval = 1, repeatIntervalUnit = QuartzDefinition.SimpleRepeatIntervalUnit.HOURS, params = {})
+    public void syncWikiToNewKnowledgeBase(Map<String, Object> map) {
+        dataMigrationService.fixData();
+    }
+}
