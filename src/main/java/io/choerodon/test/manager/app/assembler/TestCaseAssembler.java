9f37bf139b2a51e8a3b8fc2a11295e5f56c37bef
lizhaozhong <zhaozhong.li@hand-china.com>
2019-12-16 13:45:10 +0800

    [IMP] 优化代码/修复详情问题

--- src/main/java/io/choerodon/test/manager/app/assembler/TestCaseAssembler.java
+++ src/main/java/io/choerodon/test/manager/app/assembler/TestCaseAssembler.java
@@ -197,11 +197,13 @@ public class TestCaseAssembler {
         }
         // 查询附件信息
         TestCycleCaseAttachmentRelDTO testCycleCaseAttachmentRelDTO = new TestCycleCaseAttachmentRelDTO();
-        testCycleCaseAttachmentRelDTO.setAttachmentLinkId(testCycleCaseInfoVO.getExecuteId());
+            testCycleCaseAttachmentRelDTO.setAttachmentLinkId(testCycleCaseInfoVO.getExecuteId());
         testCycleCaseAttachmentRelDTO.setAttachmentType(TYPE);
         List<TestCycleCaseAttachmentRelDTO> testCycleCaseAttachmentRelDTOS = testCycleCaseAttachmentRelMapper.select(testCycleCaseAttachmentRelDTO);
         testCycleCaseInfoVO.setAttachment(modelMapper.map(testCycleCaseAttachmentRelDTOS, new TypeToken<List<TestCycleCaseAttachmentRelVO>>() {
         }.getType()));
+        // 用例的问题链接
+        testCycleCaseInfoVO.setIssuesInfos(testCaseLinkService.listIssueInfo(testCycleCaseInfoVO.getProjectId(), testCycleCaseInfoVO.getCaseId()));
         // 查询用例信息
         TestCaseDTO testCaseDTO = testCaseMapper.selectByPrimaryKey(testCycleCaseInfoVO.getCaseId());
         if(!ObjectUtils.isEmpty(testCaseDTO)){
