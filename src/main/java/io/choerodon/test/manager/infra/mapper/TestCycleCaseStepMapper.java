43f7fee90339376add901ada21b8aaeedff4cb27
zhaotianxin <ztxemail@163.com>
2019-12-20 13:55:49 +0800

    [IMP] 优化克隆计划

--- src/main/java/io/choerodon/test/manager/infra/mapper/TestCycleCaseStepMapper.java
+++ src/main/java/io/choerodon/test/manager/infra/mapper/TestCycleCaseStepMapper.java
@@ -42,4 +42,6 @@ public interface TestCycleCaseStepMapper extends Mapper<TestCycleCaseStepDTO> {
     List<TestCycleCaseStepDTO>  listByexecuteIds(@Param("ids") List<Long> olderExecuteId);
 
     List<TestCycleCaseStepDTO> selectByexecuteId(@Param("executeId") Long executeId);
+
+    int countByExecuteIds(@Param("list") List<Long> olderExecuteIds);
 }
