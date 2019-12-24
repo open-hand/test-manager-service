19120eb37258b87e46b672b35a566cf6e100396c
HuangFuqiang <fuqianghuang01@gmail.com>
2019-12-24 14:59:29 +0800

    [FIX]优化代码

--- src/main/java/io/choerodon/test/manager/app/service/impl/DataMigrationServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/app/service/impl/DataMigrationServiceImpl.java
@@ -12,7 +12,6 @@ import io.choerodon.test.manager.infra.enums.TestPlanInitStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
