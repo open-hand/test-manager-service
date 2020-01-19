9feb7413814c7d23ee5eea5e237ac3c620c69c9d
laincarl <1047972853@qq.com>
2020-01-19 14:47:56 +0800

    [FIX]修复计划更新后日历时间没有更新

--- react/routes/TestPlan/components/EventCalendar/EventCalendar.js
+++ react/routes/TestPlan/components/EventCalendar/EventCalendar.js
@@ -18,14 +18,8 @@ const moment = extendMoment(Moment);
 class EventCalendar extends Component {
   constructor(props) {
     super(props);
-    const { times } = props;
-    let baseDate = moment();
-    let endDate = moment();
-    if (times && times.length > 0) {
-      baseDate = times[0].start ? moment(times[0].start).startOf('day') : moment();
-      endDate = times[0].end ? moment(times[0].end).startOf('day') : moment();
-    }
-
+    const baseDate = moment();
+    const endDate = moment();
     this.currentDate = baseDate;
     this.state = {
       baseDate, // 显示的开始时间
@@ -35,6 +29,24 @@ class EventCalendar extends Component {
     };
   }
 
+  static getDerivedStateFromProps(nextProps, state) {
+    if (!isEqual(state.times, nextProps.times)) {
+      let baseDate = moment();
+      let endDate = moment();
+      const { times } = nextProps;
+      if (times && times.length > 0) {
+        baseDate = times[0].start ? moment(times[0].start).startOf('day') : moment();
+        endDate = times[0].end ? moment(times[0].end).startOf('day') : moment();
+      }
+
+      return {
+        baseDate, // 显示的开始时间
+        endDate, // 显示的结束时间
+      };
+    } else {
+      return null;
+    }
+  }
 
   shouldComponentUpdate(nextProps, nextState) {
     return !isEqual(this.props, nextProps) || !isEqual(this.state, nextState);
@@ -170,7 +182,12 @@ class EventCalendar extends Component {
       <Spin spinning={calendarLoading}>
         <div className="c7ntest-EventCalendar" style={{ height: showMode === 'multi' ? '100%' : '162px' }}>
           {/* 头部 */}
-          <div className="c7ntest-EventCalendar-header" style={{ marginTop: '-50px', zIndex: 100, flexDirection: 'row-reverse', display: testPlanStore.mainActiveTab === 'testPlanTable' ? 'none' : 'flex' }}>
+          <div
+            className="c7ntest-EventCalendar-header"
+            style={{
+              marginTop: '-50px', zIndex: 100, flexDirection: 'row-reverse', display: testPlanStore.mainActiveTab === 'testPlanTable' ? 'none' : 'flex', 
+            }}
+          >
             <div className="c7ntest-EventCalendar-header-title">
               <div className="c7ntest-EventCalendar-header-skip">
                 <RangePicker
