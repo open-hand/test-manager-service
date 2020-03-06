d2a473fbc1b1f31628b59253fb4ccab5543a37ae
HuangFuqiang <fuqianghuang01@gmail.com>
2020-03-06 19:25:14 +0800

    [FIX]修复测试步骤为空报错

--- src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleCaseStepServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/app/service/impl/TestCycleCaseStepServiceImpl.java
@@ -127,7 +127,11 @@ public class TestCycleCaseStepServiceImpl implements TestCycleCaseStepService {
             });
             testCycleCaseDefectRelService.populateCaseStepDefectInfo(testCycleCaseStepVOS, projectId, organizationId);
             PageInfo<TestCycleCaseStepVO> pageFromList = PageUtil.createPageFromList(testCycleCaseStepVOS, pageable);
-            return pageFromList;
+            if (pageFromList == null) {
+                return new PageInfo<>();
+            } else {
+                return pageFromList;
+            }
         } else {
             return new PageInfo<>();
         }
