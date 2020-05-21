8c4f2d3be6d76e78d640031acedad5973de9bd2b
laincarl <1047972853@qq.com>
2020-05-21 15:25:42 +0800

    [FIX]更新依赖

---
+++ babel.config.js
@@ -0,0 +1,43 @@
+const alias = require('./alias').gulp;
+module.exports = {
+  presets: [
+    '@babel/preset-react',
+    ['@babel/preset-env', {
+      modules: false,
+    }],
+  ],
+  plugins: [
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias,
+      }
+    ],
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    ['@babel/plugin-proposal-decorators', {
+      legacy: true,
+    }],
+    ['@babel/plugin-proposal-class-properties', {
+      loose: true,
+    }],
+    ['babel-plugin-import',
+      {
+        libraryName: 'choerodon-ui',
+        style: true,
+      },
+      'choerodon-ui',
+    ], ['babel-plugin-import',
+      {
+        libraryName: 'choerodon-ui/pro',
+        style: true,
+      },
+      'choerodon-ui-pro',
+    ],
+    'babel-plugin-lodash',
+    ['babel-plugin-try-import', {
+      tryImport: 'C7NTryImport',
+      hasModule: 'C7NHasModule',
+    }],
+  ],
+}
