3ab26656c59ecaec89e4323cd25c27b8b1d15ffc
zhaotianxin <ztxemail@163.com>
2020-06-05 18:30:54 +0800

    [IMP] 自动化测试手动添加createdBy和lastUpdatedBy

--- src/main/java/io/choerodon/test/manager/app/service/impl/TestAppInstanceServiceImpl.java
+++ src/main/java/io/choerodon/test/manager/app/service/impl/TestAppInstanceServiceImpl.java
@@ -301,9 +301,7 @@ public class TestAppInstanceServiceImpl implements TestAppInstanceService {
         testAppInstanceDTO.setId(Long.valueOf(TestAppInstanceDTO.getInstanceIDFromReleaseName(releaseNames)));
         TestAppInstanceLogDTO logE = new TestAppInstanceLogDTO();
         logE.setLog(logFile);
-        logE.setCreatedBy(0L);
-        logE.setLastUpdatedBy(0L);
-        if (testAppInstanceLogMapper.insert(logE) == 0) {
+        if (testAppInstanceLogMapper.insertTestAppInstanceLog(logE) == 0) {
             throw new CommonException("error.ITestAppInstanceLogServiceImpl.insert");
         }
         testAppInstanceDTO.setLogId(logE.getId());
--- src/main/java/io/choerodon/test/manager/infra/mapper/TestAppInstanceLogMapper.java
+++ src/main/java/io/choerodon/test/manager/infra/mapper/TestAppInstanceLogMapper.java
@@ -2,6 +2,8 @@ package io.choerodon.test.manager.infra.mapper;
 
 import io.choerodon.mybatis.common.BaseMapper;
 import io.choerodon.test.manager.infra.dto.TestAppInstanceLogDTO;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestAppInstanceLogMapper extends BaseMapper<TestAppInstanceLogDTO> {
+    int insertTestAppInstanceLog(@Param("logE") TestAppInstanceLogDTO logE);
 }
