85a977acc7f871722481e4cb71ddfe5ea95a5ce4
DingZhicheng <zhicheng.ding@hand-china.com>
2020-06-18 09:25:06 +0800

    [FIX]更改层次选择时的判断

--- react/routes/TestPlan/components/TestPlanModal/SelectIssueStore.js
+++ react/routes/TestPlan/components/TestPlanModal/SelectIssueStore.js
@@ -22,6 +22,7 @@ class IssueTreeStore {
   // 使用map，方便查找访问
   treeMap = observable.map();
 
+  // 用于进行根目录判断
   treeRootMap=observable.map();
 
   @action clearStore = () => {
@@ -64,6 +65,7 @@ class IssueTreeStore {
     const { rootIds, treeFolder } = treeData;
     // 选中之前选中的
     const selectedId = this.currentCycle.id || rootIds[0];
+    // 保存根目录 用于后续递归选择时判断根节点  
     rootIds.forEach((r) => {
       this.treeRootMap.set(r, true);
     });
@@ -169,7 +171,7 @@ class IssueTreeStore {
     // 如果有一个子没选中，就是中间态
     const isIndeterminate = item.checked ? children.some(childId => !this.treeMap.get(childId).checked) : false;
     item.isIndeterminate = isIndeterminate;
-    if (parentId) {
+    if (!this.treeRootMap.get(id)) {
       this.autoHandleParent(parentId, checked);
     }
   }
