a28a28145ee87c3b0b66183510d303c2c0909e77
HuangFuqiang <fuqianghuang01@gmail.com>
2020-07-16 19:25:09 +0800

    [FIX]修改liquibase配置

--- init-local-database.sh
+++ init-local-database.sh
@@ -8,12 +8,15 @@ mvn org.apache.maven.plugins:maven-dependency-plugin:get \
 -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
 -Dtransitive=false
 
-java -Dspring.datasource.url="jdbc:mysql://localhost/?useUnicode=true&characterEncoding=utf-8&useSSL=false&useInformationSchema=true&remarks=true&serverTimezone=UTC" \
--Dspring.datasource.username=choerodon \
--Dspring.datasource.password=123456 \
--Dspring.datasource.driver-class-name=com.mysql.jdbc.Driver \
--Ddata.init=true \
--Dlogging.level.root=info \
--Ddata.version=1.4.0.RELEASE \
--Dinstaller.jarPath=target/app.jar \
--jar ${TOOL_JAR_PATH}
+java -Dspring.datasource.url="jdbc:mysql://localhost:3306/?serverTimezone=CTT&useUnicode=true&characterEncoding=utf-8&useSSL=false&useInformationSchema=true&remarks=true" \
+ -Dspring.datasource.username=choerodon \
+ -Dspring.datasource.password=123456 \
+ -Dspring.datasource.driver-class-name=com.mysql.jdbc.Driver \
+ -Dinstaller.datasources.platform.url="jdbc:mysql://localhost:3306/?useUnicode=true&characterEncoding=utf-8&useSSL=false&useInformationSchema=true&remarks=true" \
+ -Dinstaller.datasources.platform.username=choerodon \
+ -Dinstaller.datasources.platform.password=123456 \
+ -Dinstaller.datasources.platform.driver-class-name=com.mysql.jdbc.Driver \
+ -Ddata.init=true \
+ -Dlogging.level.root=info \
+ -Dinstaller.jarPath=target/app.jar \
+ -jar ${TOOL_JAR_PATH}
