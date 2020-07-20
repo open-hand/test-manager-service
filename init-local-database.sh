b377bc19824ed3fca9aeebaf90d18d3d235b9dfb
scp <changping.shi@hand-china.com>
2020-07-20 10:54:03 +0800

    [IMP] 修改初始化db

--- init-local-database.sh
+++ init-local-database.sh
@@ -2,7 +2,7 @@
 MAVEN_LOCAL_REPO=$(cd / && mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
 TOOL_GROUP_ID=io.choerodon
 TOOL_ARTIFACT_ID=choerodon-tool-liquibase
-TOOL_VERSION=0.16.2-SNAPSHOT
+TOOL_VERSION=0.17.1.RELEASE
 TOOL_JAR_PATH=${MAVEN_LOCAL_REPO}/${TOOL_GROUP_ID/\./\/}/${TOOL_ARTIFACT_ID}/${TOOL_VERSION}/${TOOL_ARTIFACT_ID}-${TOOL_VERSION}.jar
 mvn org.apache.maven.plugins:maven-dependency-plugin:get \
 -Dartifact=${TOOL_GROUP_ID}:${TOOL_ARTIFACT_ID}:${TOOL_VERSION} \
