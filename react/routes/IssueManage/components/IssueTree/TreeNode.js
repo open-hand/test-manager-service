2d36b9d61964c5a92a40d34c5006d03340ee019a
laincarl <1047972853@qq.com>
2020-07-21 16:08:39 +0800

    [FIX]修复issue拖动

--- react/routes/IssueManage/components/IssueTree/TreeNode.js
+++ react/routes/IssueManage/components/IssueTree/TreeNode.js
@@ -3,10 +3,16 @@ import { observer } from 'mobx-react-lite';
 import IssueStore from '../../stores/IssueStore';
 import IssueTreeStore from '../../stores/IssueTreeStore';
 
+let task;
+export function getTask() {
+  return task;
+}
 function TreeNode({ children, item }) {
   const [dragEnter, setDragEnter] = useState(false);
   const hasChildren = item.children && item.children.length > 0;
   const canDrop = !hasChildren && IssueStore.tableDraging;
+  // mouseup和onDragEnd之间有几秒的时间差，会导致dragEnd不执行，这里mouseUp等dragEnd之后再调用mouseup
+  // https://github.com/atlassian/react-beautiful-dnd/issues/180
   const handleMouseUp = async (e) => {
     setDragEnter(false);
     const isCopy = e.ctrlKey || e.metaKey;
@@ -23,8 +29,12 @@ function TreeNode({ children, item }) {
         },
         onMouseLeave: () => {
           setDragEnter(false);
+          task = null;
         },
-        onMouseUp: handleMouseUp,
+        onMouseUp: (e) => {
+          e.persist();
+          task = () => handleMouseUp(e);
+        },
       } : {}}
     >
       {children}
