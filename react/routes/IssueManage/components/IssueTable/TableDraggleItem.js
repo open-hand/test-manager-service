e196907bc4a904d47f29cb1d74a0aed3b6cf6ad9
laincarl <1047972853@qq.com>
2020-07-21 16:08:39 +0800

    [FIX]修复issue拖动

--- react/routes/IssueManage/components/IssueTable/TableDraggleItem.js
+++ react/routes/IssueManage/components/IssueTable/TableDraggleItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import _ from 'lodash';
 import classNames from 'classnames';
-import { toJS } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { Draggable } from 'react-beautiful-dnd';
 import IssueStore from '../../stores/IssueStore';
 
+
+function getStyle(style, snapshot) {
+  if (!snapshot.isDropAnimating) {
+    return style;
+  }
+  return {
+    ...style,
+    // cannot be 0, but make it super tiny
+    transitionDuration: '0.001s',
+  };
+}
 export default observer((props) => {
   const draggingTableItems = IssueStore.getDraggingTableItems;
   const {
@@ -23,47 +33,47 @@ export default observer((props) => {
           style={{
             background: !snapshotinner.isDragging && issue.typeCode !== 'issue_auto_test' && _.find(draggingTableItems, { caseId: issue.caseId }) && 'rgb(235, 242, 249)',
             position: 'relative',
-            ...provided.draggableProps.style,
+            ...getStyle(provided.draggableProps.style, snapshotinner),
           }}
           onClick={(e) => { handleClickIssue(issue, index, e); }}
           className={classes}
         >
           {snapshotinner.isDragging
-              && (
-                <div style={{
-                  position: 'absolute',
-                  width: 20,
-                  height: 20,
-                  background: 'red',
-                  textAlign: 'center',
-                  color: 'white',
-                  borderRadius: '50%',
-                  top: 0,
-                  left: 0,
-                }}
-                >
-                  {draggingTableItems.length}
-                </div>
-              )
-            }
+            && (
+              <div style={{
+                position: 'absolute',
+                width: 20,
+                height: 20,
+                background: 'red',
+                textAlign: 'center',
+                color: 'white',
+                borderRadius: '50%',
+                top: 0,
+                left: 0,
+              }}
+              >
+                {draggingTableItems.length}
+              </div>
+            )
+          }
           {snapshotinner.isDragging
-              && (
-                <div className="IssueTable-drag-prompt">
+            && (
+              <div className="IssueTable-drag-prompt">
+                <div>
+                  复制或移动测试用例
+                </div>
+                <div> 按下ctrl/command复制</div>
+                <div
+                  ref={props.instanceRef}
+                >
                   <div>
-                    复制或移动测试用例
-                  </div>
-                  <div> 按下ctrl/command复制</div>
-                  <div
-                    ref={props.instanceRef}
-                  >
-                    <div>
-                      当前状态：
-                      <span style={{ fontWeight: 500 }}>移动</span>
-                    </div>
+                    当前状态：
+                    <span style={{ fontWeight: 500 }}>移动</span>
                   </div>
                 </div>
-              )
-            }
+              </div>
+            )
+          }
           {props.children}
         </tr>
       )
