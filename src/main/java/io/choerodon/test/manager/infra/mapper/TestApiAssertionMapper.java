ecf9db6ee3b25960d4c7ac3f2b1c20807b4a7654
lihao <310103270@qq.com>
2020-08-05 17:30:11 +0800

    [ADD] 添加执行任务接口

--- src/main/java/io/choerodon/test/manager/infra/mapper/TestApiAssertionMapper.java
+++ src/main/java/io/choerodon/test/manager/infra/mapper/TestApiAssertionMapper.java
@@ -2,6 +2,10 @@ package io.choerodon.test.manager.infra.mapper;
 
 import io.choerodon.mybatis.common.BaseMapper;
 import io.choerodon.test.manager.infra.dto.TestApiAssertionDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TestApiAssertionMapper extends BaseMapper<TestApiAssertionDTO> {
+    List<TestApiAssertionDTO> listByCaseIds(@Param("caseIds") List<Long> caseIds);
 }
