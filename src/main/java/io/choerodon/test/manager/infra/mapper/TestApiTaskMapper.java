5e2e2fe5872e27d71e3119a2e509cb6c40b5def2
lihao <310103270@qq.com>
2020-08-06 12:00:00 +0800

    [DEL] 去除api测试相关内容

--- src/main/java/io/choerodon/test/manager/infra/mapper/TestApiTaskMapper.java
+++
@@ -1,7 +0,0 @@
-package io.choerodon.test.manager.infra.mapper;
-
-import io.choerodon.mybatis.common.BaseMapper;
-import io.choerodon.test.manager.infra.dto.TestApiTaskDTO;
-
-public interface TestApiTaskMapper extends BaseMapper<TestApiTaskDTO> {
-}
--- src/main/java/io/choerodon/test/manager/infra/mapper/TestApiTaskResultInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package io.choerodon.test.manager.infra.mapper;
-
-import io.choerodon.mybatis.common.BaseMapper;
-import io.choerodon.test.manager.infra.dto.TestApiTaskResultInfoDTO;
-
-public interface TestApiTaskResultInfoMapper extends BaseMapper<TestApiTaskResultInfoDTO> {
-}
