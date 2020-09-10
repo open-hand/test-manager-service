2ddbe58f60d3d5d24f8d55239376ed76271f8755
jiaxu.cui <hand1234>
2020-09-10 11:51:54 +0800

    [FIX] 修复异步线程无法获取加密信息

--- src/main/java/io/choerodon/test/manager/app/service/ExcelImportService.java
+++ src/main/java/io/choerodon/test/manager/app/service/ExcelImportService.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.context.request.RequestAttributes;
 
 public interface ExcelImportService {
 
@@ -11,7 +12,7 @@ public interface ExcelImportService {
 
     void downloadImportTemp(HttpServletRequest request, HttpServletResponse response, Long organizationId, Long projectId);
 
-    void importIssueByExcel(Long projectId, Long folderId, Long userId, Workbook workbook);
+    void importIssueByExcel(Long projectId, Long folderId, Long userId, Workbook workbook, RequestAttributes requestAttributes);
 
     Workbook buildImportTemp(Long organizationId, Long projectId);
 }
