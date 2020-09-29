06f32a6893e337a5e629e0c15ec9b18e6ee30fff
laincarl <1047972853@qq.com>
2020-09-29 17:04:15 +0800

    [FIX]修复创建状态传递项目id

--- react/api/TestStatusApi.js
+++ react/api/TestStatusApi.js
@@ -4,10 +4,10 @@ export function getStatusList(statusType) {
   return request.post(`/test/v1/projects/${getProjectId()}/status/query`, { statusType, projectId: getProjectId() });
 }
 export function editStatus(data) {
-  return request.put(`/test/v1/projects/${getProjectId()}/status/update`, { ...data, ...{ projectId: Number(getProjectId()) } });
+  return request.put(`/test/v1/projects/${getProjectId()}/status/update`, { ...data, ...{ projectId: getProjectId() } });
 }
 export function createStatus(data) {
-  return request.post(`/test/v1/projects/${getProjectId()}/status`, { ...data, ...{ projectId: Number(getProjectId()) } });
+  return request.post(`/test/v1/projects/${getProjectId()}/status`, { ...data, ...{ projectId: getProjectId() } });
 }
 export function deleteStatus(statusId) {
   return request.delete(`/test/v1/projects/${getProjectId()}/status/${statusId}`);
