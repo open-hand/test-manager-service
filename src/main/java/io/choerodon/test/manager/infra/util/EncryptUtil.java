aba1726904af5aa6cce4cd00413dcb541047557e
25287 <chihao.ran@hand-china.com>
2021-02-25 10:40:09 +0800

    [FIX]修改EncryptUtil解析searchVO解决查询用例关联问题筛选失效

--- src/main/java/io/choerodon/test/manager/infra/util/EncryptUtil.java
+++ src/main/java/io/choerodon/test/manager/infra/util/EncryptUtil.java
@@ -6,6 +6,7 @@ import io.choerodon.test.manager.api.vo.agile.SearchDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -16,8 +17,11 @@ import org.hzero.starter.keyencrypt.core.EncryptionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -61,6 +65,188 @@ public class EncryptUtil {
         Optional<Map<String, Object>> searchArgs = Optional.ofNullable(search).map(SearchDTO::getSearchArgs);
         if (searchArgs.isPresent()) {
             decryptSa(search, searchArgs);
+        }
+        Optional<Map<String, Object>> otherArgs = Optional.ofNullable(search).map(SearchDTO::getOtherArgs);
+        if (otherArgs.isPresent()) {
+            decryptOa(search, otherArgs);
+        }
+    }
+
+    private void decryptOa(SearchDTO search, Optional<Map<String, Object>> oaMapOptional) {
+        List<String> temp;
+        String tempStr;// versionList
+        // priorityId
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("priorityId"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("priorityId",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // component
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("component"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("component",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // version
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("version"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("version",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+        // influenceVersion
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("influenceVersion"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("influenceVersion",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+        // fixVersion
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("fixVersion"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("fixVersion",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+        // sprint
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("sprint"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("sprint",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // issueIds
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("issueIds"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("issueIds",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // label
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("label"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("label",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // componentIds
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("componentIds"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("componentIds",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // feature
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("feature"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("feature",
+                    temp.stream().map(item -> encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // epic
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("epic"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("epic",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // customField
+        Object ob = oaMapOptional.map(ad -> (Object) (ad.get("customField"))).orElse(null);
+        if (!ObjectUtils.isEmpty(ob)) {
+            search.getOtherArgs().put("customField", handlerCustomField(ob,false));
+        }
+
+        // assigneeId
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("assigneeId"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("assigneeId",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // mainResponsibleIds
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("mainResponsibleIds"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("mainResponsibleIds",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        //userId
+        decryptUserId(search, oaMapOptional);
+
+        // creatorIds
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("creatorIds"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("creatorIds",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+
+        // updatorIds
+        temp = oaMapOptional.map(ad -> (List<String>) (ad.get("updatorIds"))).orElse(null);
+        if (CollectionUtils.isNotEmpty(temp)) {
+            search.getOtherArgs().put("updatorIds",
+                    temp.stream().map(item -> Arrays.asList(IGNORE_VALUES).contains(item) ? item : encryptionService.decrypt(item, BLANK_KEY)).collect(Collectors.toList()));
+        }
+    }
+
+    private Object handlerCustomField(Object value, Boolean encrypt) {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(objectMapper.writeValueAsString(value));
+            ObjectNode objectNode = (ObjectNode) jsonNode;
+            Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
+            Map<String, Object> map = new HashMap<>();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> next = fields.next();
+                JsonNode nextValue = next.getValue();
+                List<Object> objects = new ArrayList<>();
+                for (JsonNode node : nextValue) {
+                    Map<String, Object> nodeObjValue = new HashMap<>();
+                    String fieldId = node.get("fieldId").textValue();
+                    nodeObjValue.put("fieldId", encrypt ? encryptionService.encrypt(fieldId, BLANK_KEY) : encryptionService.decrypt(fieldId, BLANK_KEY));
+                    JsonNode value1 = node.get("value");
+                    if ("option".equals(next.getKey())) {
+                        List<String> list = new ArrayList<>();
+                        if (value1.isArray()) {
+                            value1.forEach(v -> {
+                                list.add(v.isNumber() ? v.textValue() : (encrypt ? encryptionService.encrypt(v.textValue(), BLANK_KEY) : encryptionService.decrypt(v.textValue(), BLANK_KEY)));
+                            });
+                        }
+                        nodeObjValue.put("value", list);
+                    } else if (StringUtils.contains(next.getKey(), "date")){
+                        try {
+                            String startTime = null;
+                            String endTime = null;
+                            if (encrypt) {
+                                String startDate = node.get("startDate").asText();
+                                String endDate = node.get("endDate").asText();
+                                startTime = StringUtils.containsAny(startDate, "-", ":")? startDate : sdf.format(new Date(Long.parseLong(startDate)));
+                                endTime = StringUtils.containsAny(endDate, "-", ":")? endDate : sdf.format(new Date(Long.parseLong(endDate)));
+                            } else {
+                                startTime = node.get("startDate").textValue();
+                                endTime = node.get("endDate").textValue();
+                            }
+                            nodeObjValue.put("startDate", startTime);
+                            nodeObjValue.put("endDate", endTime);
+                        } catch (Exception e) {
+                            throw new CommonException(e);
+                        }
+                    }else {
+                        nodeObjValue.put("value", value1.isNumber() ? value1.numberValue() : value1.textValue());
+                    }
+                    objects.add(nodeObjValue);
+                }
+                map.put(next.getKey(), objects);
+            }
+            return map;
+        } catch (IOException e) {
+            LOGGER.error("jackson io error: {}", e);
+        }
+        return null;
+    }
+
+    private void decryptUserId(SearchDTO search, Optional<Map<String, Object>> oaMapOptional) {
+        String tempStr = oaMapOptional.map(ad -> (String) ad.get("userId")).orElse(null);
+        if (!Objects.isNull(tempStr)) {
+            search.getOtherArgs().put("userId", Arrays.asList(IGNORE_VALUES).contains(tempStr) ? tempStr : encryptionService.decrypt(tempStr, BLANK_KEY));
         }
     }
 
