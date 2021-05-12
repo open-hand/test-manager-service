5cb080502a7f8b10f6d1acdc8f455ece39b553b1
20615 <wenfei.li@hand-china.com>
2021-05-12 10:46:48 +0800

    [IMP] 添加测试状态变更状态自动流转逻辑

--- react/install.js
+++ react/install.js
@@ -1,3 +1,4 @@
 import { set } from '@choerodon/inject';
 
 set('testmanager:IssueLinkedTestCase', () => import('@/injects/issue-linked-test-case'));
+set('testmanager:StatusAutoTransform', () => import('@/injects/status-autoTransform'));
