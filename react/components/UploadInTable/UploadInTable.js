905714d405476669027cc13bcd609bdbbd5a4307
DingZhicheng <zhicheng.ding@hand-china.com>
2021-12-10 13:10:36 +0800

    [FIX]测试执行步骤表格附件显示更改

--- react/components/UploadInTable/UploadInTable.js
+++ react/components/UploadInTable/UploadInTable.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import { Choerodon } from '@choerodon/boot';
 import { Upload } from 'choerodon-ui';
@@ -37,7 +39,7 @@ function UploadInTable(props) {
           <SingleFileUpload
             key={item.id}
             url={item.url}
-            fileName={item.attachmentName}
+            fileName={item.name}
             onDeleteFile={() => handleDeleteFile(item)}
             hasDeletePermission
           // hasDeletePermission={hasPermission || AppState.userInfo.id === item.userId}
