b6a6e42cef03e8df73842658c21165849a08ce77
superlee <kaiwen.li@hand-china.com>
2022-04-15 16:25:58 +0800

    [ADD] test_case_link表和test_cycle_case_history添加索引

--- src/main/resources/script/db/groovy/test_manager_service/test_cycle_case_history.groovy
+++ src/main/resources/script/db/groovy/test_manager_service/test_cycle_case_history.groovy
@@ -34,4 +34,19 @@ databaseChangeLog(logicalFilePath: 'script/script/init_tables.groovy.groovy') {
             createSequence(sequenceName: 'test_cycle_case_history_s', startValue: "1")
         }
     }
+
+    changeSet(author: 'kaiwen.li@hang-china.com', id: '2022-04-15-test-cycle-case-history-add-index') {
+        createIndex(tableName: "test_cycle_case_history", indexName: "idx_execute_id") {
+            column(name: "execute_id")
+        }
+        createIndex(tableName: "test_cycle_case_history", indexName: "idx_field") {
+            column(name: "field")
+        }
+        createIndex(tableName: "test_cycle_case_history", indexName: "idx_old_value") {
+            column(name: "old_value(10)")
+        }
+        createIndex(tableName: "test_cycle_case_history", indexName: "idx_new_value") {
+            column(name: "new_value(10)")
+        }
+    }
 }
