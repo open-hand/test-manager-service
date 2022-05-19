2c7e0941a9e10c066313490e76a3a11d08180b89
DingZhicheng <ding25410@foxmail.com>
2022-05-19 18:52:44 +0800

    [IMP] 上传附件组件更换为敏捷上传附件

--- react/components/create-test-case/UploadButton.js
+++ react/components/create-test-case/UploadButton.js
@@ -1,33 +1,3 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect } from 'react';
-import { Upload } from 'choerodon-ui';
-import { Button } from 'choerodon-ui/pro';
-
-import { randomWord } from '@/common/utils';
-import './UploadButton.less';
-
-const UploadButton = ({ ...props }) => {
-  const className = randomWord(false, 32);
-  useEffect(() => {
-    const uploadElement = document.querySelector(`.${className} .c7n-upload-select`);
-    const uploadListElement = document.querySelector(`.${className} .c7n-upload-list`);
-    if (uploadElement && uploadListElement) {
-      uploadListElement.appendChild(uploadElement);
-    }
-  });
-  const innerProps = {
-    multiple: true,
-    beforeUpload: () => false,
-  };
-  return (
-    <Upload
-      {...innerProps}
-      {...props}
-      className={`c7nagile-upload-button ${className}`}
-    >
-      <Button funcType="raised" icon="file_upload_black-o">上传附件</Button>
-    </Upload>
-  );
-};
+import UploadButton from '@choerodon/agile/lib/components/CommonComponent/UploadButton';
 
 export default UploadButton;
