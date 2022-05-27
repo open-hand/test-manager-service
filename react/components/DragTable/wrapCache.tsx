160eaae0aeb471fe9e6d4eb79097744f60b86ed4
DingZhicheng <ding25410@foxmail.com>
2022-05-27 09:27:08 +0800

    [IMP] 用例库、计划表格默认隐藏自定义编号列

--- react/components/DragTable/wrapCache.tsx
+++ react/components/DragTable/wrapCache.tsx
@@ -3,19 +3,20 @@ import { useSimpleUpdateColumnCache } from '@/hooks/data/useTableColumns';
 /**
  *  注入更新表格列方法
  */
-function wrapDragTableCache(Element: any, type: string) {
+function wrapDragTableCache(Element: any, type: string, defaultVisibleColumnCodes?: string[]) {
   function DragColumnCacheTable(props: any) {
     const { tableRef, cached } = props;
-    const columnCodes = props.columns?.map((column: any) => column.key);
+    const hiddenColumnCodes = ['checkbox', 'more', 'action'];
+    const columnCodes = props.columns?.map((column: any) => column.key).filter((columnCode:string) => !hiddenColumnCodes.includes(columnCode));
     const { updateColumnCache } = useSimpleUpdateColumnCache(type, columnCodes);
     const handleColumnFilterChange = useCallback((visibleColumns) => {
       updateColumnCache(visibleColumns);
     }, [updateColumnCache]);
     const { listLayoutColumns } = cached || {};
     if (!listLayoutColumns) {
-      return <Element ref={tableRef} {...props} onColumnFilterChange={handleColumnFilterChange} />;
+      return <Element ref={tableRef} {...props} selectedKeys={defaultVisibleColumnCodes} onColumnFilterChange={handleColumnFilterChange} />;
     }
-    const displayColumns = listLayoutColumns.filter((column:any) => column.display).map((column:any) => column.columnCode);
+    const displayColumns = listLayoutColumns.filter((column: any) => column.display).map((column: any) => column.columnCode);
     const defaultFilteredColumns = !displayColumns.length ? ['summary'] : displayColumns;
     return <Element ref={tableRef} selectedKeys={defaultFilteredColumns} {...props} onColumnFilterChange={handleColumnFilterChange} />;
   }
